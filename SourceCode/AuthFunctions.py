92be2d5d6a2425fb23667522610ce3e48e8fd4a9
Carlos Mbendera <carlosmbendera@gmail.com>
2025-10-22 12:59:03 -0500

    Moved files

---
+++ SourceCode/AuthFunctions.py
@@ -0,0 +1,171 @@
+import json
+import requests
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+#This code is from a project Carlos wrote a while a go for a research project and provides a starting point for the team to work off
+
+## TODO: ADD USE STREAMLIT KEYS IF NOT RUNNING LOCALLY 
+# Initialize Firebase if not already initialized
+# if not firebase_admin._apps:
+#     # cred = credentials.Certificate('firebaseKEYAFSTDUY.json')
+#     # firebase_admin.initialize_app(cred)
+
+# db = firestore.client()
+
+## -------------------------------------------------------------------------------------------------
+## Firebase Auth API -------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+
+def sign_in_with_email_and_password(email, password):
+    request_ref = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key={0}".format(st.secrets['FIREBASE_WEB_API_KEY'])
+    headers = {"content-type": "application/json; charset=UTF-8"}
+    data = json.dumps({"email": email, "password": password, "returnSecureToken": True})
+    request_object = requests.post(request_ref, headers=headers, data=data)
+    raise_detailed_error(request_object)
+    return request_object.json()
+
+def get_account_info(id_token):
+    request_ref = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getAccountInfo?key={0}".format(st.secrets['FIREBASE_WEB_API_KEY'])
+    headers = {"content-type": "application/json; charset=UTF-8"}
+    data = json.dumps({"idToken": id_token})
+    request_object = requests.post(request_ref, headers=headers, data=data)
+    raise_detailed_error(request_object)
+    return request_object.json()
+
+def send_email_verification(id_token):
+    request_ref = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getOobConfirmationCode?key={0}".format(st.secrets['FIREBASE_WEB_API_KEY'])
+    headers = {"content-type": "application/json; charset=UTF-8"}
+    data = json.dumps({"requestType": "VERIFY_EMAIL", "idToken": id_token})
+    request_object = requests.post(request_ref, headers=headers, data=data)
+    raise_detailed_error(request_object)
+    return request_object.json()
+
+def send_password_reset_email(email):
+    request_ref = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getOobConfirmationCode?key={0}".format(st.secrets['FIREBASE_WEB_API_KEY'])
+    headers = {"content-type": "application/json; charset=UTF-8"}
+    data = json.dumps({"requestType": "PASSWORD_RESET", "email": email})
+    request_object = requests.post(request_ref, headers=headers, data=data)
+    raise_detailed_error(request_object)
+    return request_object.json()
+
+def create_user_with_email_and_password(email, password, first_name=None):
+    request_ref = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key={0}".format(st.secrets['FIREBASE_WEB_API_KEY'])
+    headers = {"content-type": "application/json; charset=UTF-8"}
+    data = {"email": email, "password": password, "returnSecureToken": True}
+    if first_name:
+        data["displayName"] = first_name
+    request_object = requests.post(request_ref, headers=headers, data=json.dumps(data))
+    raise_detailed_error(request_object)
+    return request_object.json()
+
+def delete_user_account(id_token):
+    request_ref = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/deleteAccount?key={0}".format(st.secrets['FIREBASE_WEB_API_KEY'])
+    headers = {"content-type": "application/json; charset=UTF-8"}
+    data = json.dumps({"idToken": id_token})
+    request_object = requests.post(request_ref, headers=headers, data=data)
+    raise_detailed_error(request_object)
+    return request_object.json()
+
+def raise_detailed_error(request_object):
+    try:
+        request_object.raise_for_status()
+    except requests.exceptions.HTTPError as error:
+        raise requests.exceptions.HTTPError(error, request_object.text)
+
+## -------------------------------------------------------------------------------------------------
+## Authentication functions ------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+
+def sign_in(email:str, password:str) -> None:
+    try:
+        # Attempt to sign in with email and password
+        id_token = sign_in_with_email_and_password(email,password)['idToken']
+
+        # Get account information
+        user_info = get_account_info(id_token)["users"][0]
+        st.session_state.session_id = user_info['localId']
+        st.session_state.user_info = user_info
+
+        # Load chat history and reset display flag
+        st.session_state.chat_history = get_user_chat_history(user_info['localId'])
+        st.session_state._chat_history_displayed = False
+
+        st.rerun()
+
+    except requests.exceptions.HTTPError as error:
+        error_message = json.loads(error.args[1])['error']['message']
+        if error_message in {"INVALID_EMAIL","EMAIL_NOT_FOUND","INVALID_PASSWORD","MISSING_PASSWORD"}:
+            st.session_state.auth_warning = error_message
+        else:
+            st.session_state.auth_warning = 'Error: Please try again later'
+
+    except Exception as error:
+        print(error)
+        st.session_state.auth_warning = 'Error: Please try again later'
+
+
+def create_account(email: str, password: str, first_name: str) -> None:
+    try:
+        result = create_user_with_email_and_password(email, password, first_name)
+        id_token = result['idToken']
+        send_email_verification(id_token)
+        st.session_state.auth_success = 'Check your inbox to verify your email'
+
+        user_info = get_account_info(id_token)["users"][0]
+        st.session_state.session_id = user_info['localId']
+        st.session_state.user_info = user_info  # Directly set user as authenticated
+        st.session_state.auth_success = True
+
+    except requests.exceptions.HTTPError as error:
+        error_message = json.loads(error.args[1])['error']['message']
+        if error_message == "EMAIL_EXISTS":
+            st.session_state.auth_warning = 'Error: Email belongs to existing account'
+        elif error_message in {"INVALID_EMAIL","INVALID_PASSWORD","MISSING_PASSWORD","MISSING_EMAIL","WEAK_PASSWORD"}:
+            st.session_state.auth_warning = error_message
+        else:
+            st.session_state.auth_warning = error_message
+    except Exception as error:
+        st.session_state.auth_warning = error
+
+def reset_password(email:str) -> None:
+    try:
+        send_password_reset_email(email)
+        st.session_state.auth_success = 'Password reset link sent to your email'
+    
+    except requests.exceptions.HTTPError as error:
+        error_message = json.loads(error.args[1])['error']['message']
+        if error_message in {"MISSING_EMAIL","INVALID_EMAIL","EMAIL_NOT_FOUND"}:
+            st.session_state.auth_warning = 'Error: Use a valid email'
+        else:
+            st.session_state.auth_warning = 'Error: Please try again later'    
+    
+    except Exception:
+        st.session_state.auth_warning = 'Error: Please try again later'
+
+
+
+
+def sign_out() -> None:
+    st.session_state.clear()
+    st.session_state.auth_success = 'You have successfully signed out'
+
+
+def delete_account(password:str) -> None:
+    try:
+        # Confirm email and password by signing in (and save id_token)
+        id_token = sign_in_with_email_and_password(st.session_state.user_info['email'],password)['idToken']
+        
+        # Attempt to delete account
+        delete_user_account(id_token)
+        st.session_state.clear()
+        st.session_state.auth_success = 'You have successfully deleted your account'
+
+    except requests.exceptions.HTTPError as error:
+        error_message = json.loads(error.args[1])['error']['message']
+        print(error_message)
+
+    except Exception as error:
+        print(error)
+
